8a5ed8500e044e32beb5670232f2c546da8df9b7
nishant8272 <nishantverma01042005@gmail.com>
2025-08-20 01:09:50 +0530

    backend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import job from "./config/cron.js"
 import { initializeDatabase } from './config/db.js'; 
 import rateLimiter from './middleware/rateLimiter.js';
 import router from './routes/transaction.js'; // Adjust the path as necessary
@@ -9,11 +10,19 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV==="production") job.start()
 app.use(rateLimiter); // Apply rate limiting middleware
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/transactions", router);
+
+app.get("/api/health",(req,res)=>{
+ res.status(200).json({
+     status: "ok"
+ })
+})
 
 initializeDatabase().then(() => {
 app.listen(PORT, () => { 
